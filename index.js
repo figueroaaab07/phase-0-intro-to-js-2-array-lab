0e84945bcd4b8d894ac35588d77680b87e232c1d
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-03-16 12:22:02 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return;
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat(name) {
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1);
+}
